149d88541d0006eb2374fa9111d6e236eba456e5
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-09-23 18:36:39 +0530

    added logger to log project

--- src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
@@ -1,5 +1,7 @@
 package com.demo.springconcepts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,13 +11,13 @@ import com.demo.springconcepts.controller.HelloRestController;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
-
+	public static final Logger logger = LoggerFactory.getLogger(SpringBootApplication.class);
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Concepts Demo");
+		logger.debug("Welcome to Spring Concepts Demo");
 		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
 		DemoBean demoBean = context.getBean(DemoBean.class);
-		System.out.println("Demo Bean"+demoBean);
-		System.out.println(context.getBean(HelloRestController.class));
+		logger.debug("Demo Bean"+demoBean.toString());
+		//System.out.println(context.getBean(HelloRestController.class));
 	}
 
 }
